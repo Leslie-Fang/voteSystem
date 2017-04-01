b20aed4cf3ec922a9ea8ccaa818225ad597db77b
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-04-01 15:49:38 +0800

    add docker mysql

--- config.js
+++ config.js
@@ -16,4 +16,19 @@ var production_config = {
         database : 'express2react'
     }
 };
-module.exports = process.env.NODE_ENV === "production" ? production_config:dev_config;
+var docker_config = {
+    port:3388,
+    databases_config:{
+        host     : 'localhost',
+        user     : 'sampadm',
+        password : 'secret',
+        database : 'express2react'
+    }
+};
+
+    if (process.env.NODE_ENV === "production")
+        module.exports = production_config;
+    else if (process.env.NODE_ENV === "docker")
+        module.exports = docker_config;
+    else
+        module.exports = dev_config;
